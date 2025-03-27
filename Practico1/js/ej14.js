7a23684a6636cb25c3d6f60b9474c41929fce705
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2025-03-27 14:06:27 -0300

    ej14

---
+++ Practico1/js/ej14.js
@@ -0,0 +1,11 @@
+/* Crear un botón y un párrafo de texto. Al presionar el botón incrementar un contador de tres en tres. Mostrar el valor actual del contador en el párrafo cada vez que se presione el botón. */
+
+let counter = 0;
+let result;
+
+document.querySelector("#btnCount").addEventListener("click", showCounter);
+
+function showCounter(){
+    counter+=3;
+    document.querySelector("#pCount").innerHTML = `Clicks: ${counter}`;
+};
