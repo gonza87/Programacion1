6d74c2d6fe4b90ddebe973378e401cf1752ccaa7
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2025-04-02 12:16:29 -0300

    ej1

--- Practico2/js/ej1.js
+++ Practico2/js/ej1.js
@@ -1,1 +1,20 @@
-console.log("test");
+/* ⭐Ingresar un número e indicar si es positivo o negativo.
+El número será positivo si es mayor o igual a cero. */
+
+let number;
+let result;
+
+document.querySelector("#btnShow").addEventListener("click", showResult);
+
+function showResult() {
+  result = "";
+  number = Number(document.querySelector("#nbr").value);
+
+  if (number >= 0) {
+    result = "El numero es Positivo";
+  } else {
+    result = "El numero es Negativo";
+  }
+
+  document.querySelector("#pResult").innerHTML = result;
+}
