6e33eb79ab253a1e3c51fdb6c2ada8f1dc8419c8
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2025-04-02 13:31:25 -0300

    ej3

---
+++ Practico2/js/ej3.js
@@ -0,0 +1,18 @@
+/* Ingresar un número y presionar un botón, al presionar el botón, si el número es mayor que 20 mostrar "es mayor que 20", sino mostrar "es menor o igual a 20". */
+
+let number;
+let msj;
+let result;
+
+document.querySelector("#btnShow").addEventListener("click", showResult);
+
+function showResult() {
+  number = Number(document.querySelector("#nbr").value);
+  if (number > 20) {
+    msj = `es <strong>mayor</strong> a 20`;
+  } else {
+    msj = `es <strong>menor o igual</strong> a 20`;
+  }
+
+  document.querySelector("#pResult").innerHTML = msj;
+}
