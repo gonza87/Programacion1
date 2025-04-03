85b2703b936bc9d1295b8f78bcb29e246d5a019a
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2025-04-02 22:28:39 -0300

    ej9

---
+++ Practico2/js/ej9.js
@@ -0,0 +1,17 @@
+/* Se solicita una aplicación que ayude al usuario a la toma de decisiones al momento de salir de su casa.
+Los días domingo el usuario no trabaja, el resto de los días sí.
+Si la temperatura está por debajo de los 10 grados el usuario debe "abrigarse mucho", si la temperatura está por encima de los 20 grados el usuario deberá "ponerse ropa cómoda" y para casos intermedios usará "abrigo moderado". 
+Se ingresa la temperatura y el día de la semana y la aplicación deberá mostrar la salida. A la salida se le incorporará para todos los casos la instrucción de levantarse:
+
+Ejemplos de salida:
+Ej. 1:
+Levantarse
+Ponerse ropa cómoda
+Quedarse en casa, hoy no trabaja
+
+Ej. 2:
+Levantarse
+Abrigarse Mucho
+Ir al trabajo
+
+Prestar atención a que las tres acciones no tienen vinculación entre sí. */
