d5a4550fc3fa7bc94ab81fc26009844b2743bf44
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2025-04-23 13:37:38 -0300

    ej11

--- Practico3/js/ej11.js
+++ Practico3/js/ej11.js
@@ -2,26 +2,25 @@
 
 document.querySelector("#btnShow").addEventListener("click", showProduct);
 
-function showProduct(){
-    let nbrInicio = Number(document.querySelector("#nbr1").value);
-    let nbrFin = Number(document.querySelector("#nbr2").value);
-    let nbr3 = Number(document.querySelector("#nbr3").value);
-    let msj = "";
+function showProduct() {
+  let nbrInicio = Number(document.querySelector("#nbr1").value);
+  let nbrFin = Number(document.querySelector("#nbr2").value);
+  let nbr3 = Number(document.querySelector("#nbr3").value);
+  let msj = "";
 
-    if (nbrInicio > nbrFin) {
-        let aux;
-        aux = nbrInicio;
-        nbrInicio = nbrFin;
-        nbrFin = aux;
-      }
+  if (nbrInicio > nbrFin) {
+    let aux;
+    aux = nbrInicio;
+    nbrInicio = nbrFin;
+    nbrFin = aux;
+  }
 
-    while(nbrInicio <= nbrFin){
-        if(nbrInicio % nbr3 === 0){
-            msj += `${nbrInicio} es multiplo de ${nbr3} <br>`
-        }
-        nbrInicio ++;
+  while (nbrInicio <= nbrFin) {
+    if (nbrInicio % nbr3 === 0) {
+      msj += `${nbrInicio} es multiplo de ${nbr3} <br>`;
     }
+    nbrInicio++;
+  }
 
-    document.querySelector("#pResult").innerHTML = msj;
-
+  document.querySelector("#pResult").innerHTML = msj;
 }
