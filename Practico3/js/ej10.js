e1f75fb911dd927182639673a417e513a3a72d32
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2025-04-23 13:11:37 -0300

    ej10

--- Practico3/js/ej10.js
+++ Practico3/js/ej10.js
@@ -2,19 +2,16 @@
 
 document.querySelector("#btnShow").addEventListener("click", showProduct);
 
-function showProduct(){
-    let msj ="";
-    let nbr1 = Number(document.querySelector("#nbr1").value);
-    let nbr2 = Number(document.querySelector("#nbr2").value);
-    let producto=1;
-    if(nbr1 <= nbr2){
-        for(let i = nbr1; i<=nbr2; i++){
-            producto *= i;
-        }
-
+function showProduct() {
+  let nbr1 = Number(document.querySelector("#nbr1").value);
+  let nbr2 = Number(document.querySelector("#nbr2").value);
+  let producto = 1;
+  if (nbr1 <= nbr2) {
+    for (let i = nbr1; i <= nbr2; i++) {
+      producto *= i;
     }
-    else{
-        producto = "El primer numero debe ser menor al segundo"
-    }
-    document.querySelector("#pResult").innerHTML = producto
+  } else {
+    producto = "El primer numero debe ser menor al segundo";
+  }
+  document.querySelector("#pResult").innerHTML = producto;
 }
