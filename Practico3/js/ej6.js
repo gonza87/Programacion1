afda05f6297152236d5833148cf4156b2d94a176
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2025-04-23 09:14:06 -0300

    ej6

--- Practico3/js/ej6.js
+++ Practico3/js/ej6.js
@@ -1,1 +1,15 @@
 /* Imprimir todos los números múltiplos de 4 que hay entre -33 y 230. */
+
+document.querySelector("#btnShow").addEventListener("click", showMultiples);
+
+function showMultiples(){
+    let i = -33;
+    let msj = "";
+    while(i <= 230){
+        if(i % 4 === 0){
+            msj += `${i} es multiplo de 4 <br>`
+        }
+        i++;
+    }
+    document.querySelector("#pResult").innerHTML = msj;
+}
