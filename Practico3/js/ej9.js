359024e242f2ead09d06bad475c6bec52d808711
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2025-04-23 10:36:56 -0300

    ej9

--- Practico3/js/ej9.js
+++ Practico3/js/ej9.js
@@ -4,16 +4,11 @@ document.querySelector("#btnShow").addEventListener("click", showProduct);
 
 function showProduct() {
   console.log("click");
-  let msj = "";
+  
   let nbr1 = Number(document.querySelector("#nbr1").value);
   let nbr2 = Number(document.querySelector("#nbr2").value);
   let acumulador = 0;
-  if (nbr1 > nbr2) {
-    let aux;
-    aux = nbr1;
-    nbr1 = nbr2;
-    nbr2 = aux;
-  }
+
 
   for (let i = 1; i <= nbr2; i++) {
     acumulador += nbr1;
