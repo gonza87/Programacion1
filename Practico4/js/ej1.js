56ea97e7c63e0621914bf9a557fc97ab48ea1007
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2025-04-29 11:08:20 -0300

    ej1

---
+++ Practico4/js/ej1.js
@@ -0,0 +1,19 @@
+/* Solicitar un texto e indicar si comienza y termina con la misma letra. */
+
+document.querySelector("#btnShow").addEventListener("click", mostrarLetra);
+
+function mostrarLetra() {
+  let texto = document.querySelector("#txt").value;
+  texto.toLowerCase();
+  let msj = "";
+
+  let priLetra = texto.charAt(0);
+  let ultLetra = texto.charAt(texto.length - 1);
+  
+  if (priLetra === ultLetra) {
+    msj = `comienza y termina con ${priLetra}`;
+  } else {
+    msj = "NO coincide primer letra con la ultima";
+  }
+  document.querySelector("#pResult").innerHTML = msj;
+}
